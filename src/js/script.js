f29648535755cb61ae4dc4bacc547d4d158ac8c3
Vlada <vlada_chvanova@mail.ru>
2020-07-22 13:18:24 +0300

    написали цикл и я сама написала условие с помощью switch

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,75 @@
-// const num = 50;
-// // if (num < 49) {
-// //     console.log('Error');
-// // } else if (num > 100) {
-// //     console.log("Слишком много");
-// // } else {
-// //     console.log('OK');
-// // }
-// switch (num) {
-//     case 49:
-//         console.log('неверно');
-//         break;
-//     case 100:
-//         console.log('неверно');
-//         break;
-//     case 50:
-//         console.log('верно');
-//         break;
-//     default:
-//         console.log('Не в этот раз');
-// }
-let num = 50;
+// 1 практика
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+//     a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {
 
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        // break; //прерывает цикл, когда он достиг нужного значения
-        // continue; //не прерывает цикл, но пропускает указанное не нужное значение
+//     },
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+
+
+//  2 ПРАКТИКА
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// a = prompt('Один из последних просмотренных фильмов?', ''),
+// b = prompt('На сколько оцените его?', ''),
+// c = prompt('Один из последних просмотренных фильмов?', ''),
+// d = prompt('На сколько оцените его?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+
+    } else {
+        console.log('error');
+        i--;
     }
+
 }
+
+
+
+personalMovieDB.count = numberOfFilms;
+switch (true) {
+    case personalMovieDB.count > 0 && personalMovieDB.count < 10:
+        console.log('Просмотрено довольно мало фильмов');
+        break;
+    case personalMovieDB.count >= 10 && personalMovieDB.count < 30:
+        console.log('Вы классический зритель');
+        break;
+    case personalMovieDB.count > 30:
+        console.log('Вы киноман');
+        break;
+    default:
+        console.log('Произошла ошибка');
+
+
+
+
+}
+console.log(personalMovieDB);
