7f9ea6f60d9756eb60abeaaf84dadf261ecafb5d
Vlada <vlada_chvanova@mail.ru>
2020-07-27 12:55:54 +0300

    ПЕРЕДАЧА ПО ССЫЛКЕ ИЛИ ПО ЗНАЧЕНИЮ. SPREAD ОПЕРАТОР

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // const num = 50;
 // // if (num < 49) {
 // //     console.log('Error');
@@ -183,3 +183,86 @@
 // const products = str.split(', ');
 // products.sort();
 // console.log(products.join('; '));
+
+// ПЕРЕДАЧА ПО ССЫЛКЕ ИЛИ ПО ЗНАЧЕНИЮ. SPREAD ОПЕРАТОР
+
+// let a = 5,
+//   b = a;
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//   a: 5,
+//   b: 1,
+// };
+
+// const copy = obj; // ссылка на существующий объект
+
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//   let objCopy = {};
+//   let key;
+//   for (key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
+
+//   return objCopy;
+// }
+// const numbers = {
+//   a: 2,
+//   b: 5,
+//   c: {
+//     x: 7,
+//     y: 4,
+//   },
+// };
+
+// const newNumbers = copy(numbers);
+// newNumbers.a = 10;
+// // console.log(newNumbers);
+// // console.log(numbers);
+
+// const add = {
+//   d: 17,
+//   e: 20,
+// };
+
+// const clone = Object.assign({}, add);
+// clone.d = 20;
+
+// // console.log(add);
+// // console.log(clone);
+
+// const oldArray = ["a", "b", "c"];
+// const newArray = oldArray.slice();
+// newArray[1] = "myp";
+// // console.log(newArray);
+// // console.log(oldArray);
+
+// const video = ["youtube", "vimeo", "rutube"],
+//   blogs = ["wordpress", "livejournal", "blogger"],
+//   internet = [...video, ...blogs, "vk", "facebook"];
+// console.log(internet);
+
+// function log(a, b, c) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// const num = [2, 5, 7];
+// log(...num);
+
+// const array = ["a", "b"];
+// const newAarray = [...array];
+
+// const q = {
+//   one: 1,
+//   two: 2,
+// };
+
+// const newObj = { ...q };
