add6808b67f8c8855052f3f8be0816cfd6e7d22a
Vlada <vlada_chvanova@mail.ru>
2020-09-14 17:53:14 +0300

    конвертер(из рублей в доллары)

---
+++ TestWork/script.js
@@ -0,0 +1,20 @@
+const inputRub = document.querySelector('#rus'),
+    inputUsd = document.querySelector('#usd')
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest()
+
+    request.open('GET', 'test.json')
+
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send()
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState == 4 && request.status == 200) {
+            const data = JSON.parse(request.response)
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2)
+
+        } else {
+            inputUsd.value = 'Что-то пошло не так'
+        }
+    })
+})
