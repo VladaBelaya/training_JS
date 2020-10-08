2939088f5126fe5f57356e2407d53a5351e75f98
Vlada <vlada_chvanova@mail.ru>
2020-10-08 21:12:35 +0300

    1 задание по функциям

--- LearnJS/script.js
+++ LearnJS/script.js
@@ -675,3 +675,11 @@ pers.forEach((chemp) => {
 //     console.log(i);
 //   }
 // }
+
+// функции
+// Напишите функцию hello1(), которая при вызове будет возвращать строку «Привет, JavaScript!».
+
+function hello1() {
+  alert("Привет, JavaScript!");
+}
+hello1();
