cea78c68a2264745c4957516a5ad33338214e45a
Vlada <vlada_chvanova@mail.ru>
2020-10-21 21:40:35 +0300

    дано число 1-12. вывести какой период года в зависимости от числа(зима лето и тд)

--- LearnJS/script.js
+++ LearnJS/script.js
@@ -1981,3 +1981,19 @@ let test = 0
 // } else {
 //   console.log('Не делится на 3 без остатка')
 // }
+
+// Практика на условия if-else в JavaScript
+// 
+
+// В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+
+// let month = 11
+//  if (month >= 1 && month <= 2 || month == 12) {
+//    console.log('зима')
+//  } else if(month >= 3 && month <= 5) {
+//   console.log('весна')
+//  } else if (month >= 6 && month <= 8) {
+//    console.log('лето')
+//  } else if (month >= 9 && month <= 11) {
+//    console.log('осень')
+//  }
