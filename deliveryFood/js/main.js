a99755e745d190fddee44caf3ae999808451d24f
Vlada <vlada_chvanova@mail.ru>
2020-10-27 13:37:58 +0300

    сделали модальное окно(открытие и закрытие на крестик

--- deliveryFood/js/main.js
+++ deliveryFood/js/main.js
@@ -1,10 +1,24 @@
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const Close = document.querySelector(".close");
 
 cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
+Close.addEventListener("click", toggleModal);
 
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+
+// day 1
+const buttonAuth = document.querySelector('.button-auth'),
+      modalAuth =  document.querySelector('.modal-auth'),
+      closeAuth = document.querySelector('.close-auth')
+
+buttonAuth.addEventListener('click', toggleModalAuth)
+closeAuth.addEventListener('click', toggleModalAuth);
+      
+function toggleModalAuth () {
+  modalAuth.classList.toggle('is-open')
+}
+
