8df975eb81223686a558a1536a7a83d95c6d9412
Vlada <vlada_chvanova@mail.ru>
2020-11-04 14:15:08 +0300

    нахождение суммы квадратов элементов массива

--- LearnJS/script.js
+++ LearnJS/script.js
@@ -2338,3 +2338,13 @@ let test = 0
 //   result += arr[i]
 // }
 // console.log(result)
+
+// Дан массив с элементами 1, 2, 3, 4, 5. С помощью цикла for найдите сумму квадратов элементов этого массива.
+// let arr = [1, 2, 3, 4, 5]
+// let result = 0
+
+// for (let i = 0; i < arr.length; i++) {
+//   result += (arr[i] ** 2)
+// }
+
+// console.log(result)
