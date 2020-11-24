c6fd136740111c813c6cc1a8384ff963cc7006ca
Vlada <vlada_chvanova@mail.ru>
2020-11-24 17:31:47 +0300

    действия с элементами на странице. append(), prepend(), innerHTML, insertAdjacentHTML() and dr

--- LearnJS/script.js
+++ LearnJS/script.js
@@ -450,22 +450,60 @@ const personalMovieDB = {
 
 // получение элементов со страницы
 
-// const box = document.getElementById('box');
 // console.log(box);
 
-// const btns = document.getElementsByTagName('button')
 // console.log(btns[2])
 
-// const circles = document.getElementsByClassName('circle')
 // console.log(circles[1])
 
-// const hearts = document.querySelectorAll('.heart')
 // hearts.forEach(item => console.log(item))
-// const oneHeart = document.querySelector('.heart')
 // console.log(oneHeart)
 
+// действия с элементами на странице
+
+const box = document.getElementById('box');
+const btns = document.getElementsByTagName('button')
+const circles = document.getElementsByClassName('circle')
+const wrapper = document.querySelector('.wrapper')
+const hearts = wrapper.querySelectorAll('.heart')
+const oneHeart = wrapper.querySelector('.heart')
+
+// box.style.backgroundColor = 'blue'
+// box.style.width = '320px'
+box.style.cssText = 'background-color: blue; width: 320px'
+
+btns[1].style.borderRadius = '50%'
+circles[0].style.backgroundColor = 'red'
 
 
+// for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = 'blue'
+// }
+
+hearts.forEach(item => item.style.backgroundColor = 'blue')
+
+const div = document.createElement('div')
+// const text = document.createTextNode('hello')
+
+div.classList.add('black')
+wrapper.append(div)
+// wrapper.appendChild(div)
 
 
+// wrapper.prepend(div)
+
+// hearts[0].before(div)
+// hearts[0].after(div)
+// wrapper.insertBefore(div, hearts[0])
+
+// circles[0].remove()
+// wrapper.removeChild(hearts[1])
+
+// hearts[0].replaceWith(circles[0])
+// wrapper.replaceChild(circles[0], hearts[0])
+
+div.innerHTML = `<h1>Hello, world</h1>`
+// div.textContent = 'hello'
+
+div.insertAdjacentHTML('beforeend', '<h2>hello!!</h2>')
 
